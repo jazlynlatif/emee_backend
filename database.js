4c5288f53f415f67a000c4577c50b1342c56c140
jazlynlatif <93916383+jazlynlatif@users.noreply.github.com>
2026-01-05 08:49:50 +0700

    Update database.js

--- database.js
+++ database.js
@@ -16,7 +16,11 @@ console.log(
 
 
 export const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   
   // Aiven requires SSL. This tells mysql2 to use the certificate
   ssl: {
